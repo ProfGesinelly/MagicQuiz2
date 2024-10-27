81256f18cc55f012c7988d1b3f2b5ece3dc5889c
Prof Gesinelly <106716290+ProfGesinelly@users.noreply.github.com>
2024-10-27 14:45:49 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,183 @@
+const perguntas = [
+
+    {
+        enunciado: "Questão Matemática: Qual das alternativas corresponde à equação correta de uma função de 1º grau?",
+        alternativas: [
+            {
+                texto: "𝑦=2𝑥2−3 / y=2x 2−3 ",
+                afirmacao: "",
+                pontos: 0
+            },
+            {
+                texto: "𝑦=5𝑥+1 / y=5x+1",
+                afirmacao: "",
+                pontos: 200
+            }
+        ]
+    },
+    {
+        enunciado: "Qual é o valor de x na equação 3x+7=16?",
+        alternativas: [
+            {
+                texto: "2",
+                afirmacao: "",
+                pontos: 0
+            },
+            {
+                texto: "3",
+                afirmacao: "",
+                pontos: 200
+            }
+        ]
+    },
+    {
+        enunciado: " Em relação às figuras de linguagem, qual é a definição correta de metáfora?",
+        alternativas: [
+            {
+                texto: "Uso de palavras que imitam sons.",
+                afirmacao: "",
+                pontos: 0
+            },
+            {
+                texto: "Substituição de um termo por outro com o qual possui uma relação de semelhança implícita.",
+                afirmacao: "",
+                pontos: 200
+            }            
+        ]
+    },
+    {
+        enunciado: "Na estrutura de um período composto por subordinação, uma oração subordinada substantiva objetiva direta desempenha que função?",
+        alternativas: [
+            {
+                texto: "Predicativo.",
+                afirmacao: "",
+                pontos: 200
+            },
+            {
+                texto: "Objeto direto.",
+                afirmacao: "",
+                pontos: 0
+            }
+        ]
+    },
+    {
+        enunciado: "Segundo Sócrates, qual era o principal objetivo da filosofia?",
+        alternativas: [
+            {
+                texto: "Acumular conhecimento científico.",
+                afirmacao: "",
+                pontos: 1
+            },
+            {
+                texto: " Buscar a verdade por meio do questionamento contínuo.",
+                afirmacao: "",
+                pontos: 0
+            }
+        ]
+    },
+    {
+        enunciado: "Para Platão, o mundo sensível é",
+        alternativas: [
+            {
+                texto: "O único mundo verdadeiro e imutável.",
+                afirmacao: "",
+                pontos: 0
+            },
+            {
+                texto: "Uma ilusão que esconde o mundo das ideias.",
+                afirmacao: "",
+                pontos: 200
+            }
+        ]
+    },
+    {
+        enunciado: "O que define um movimento uniforme? ",
+        alternativas: [
+            {
+                texto: "Velocidade constante e aceleração variável.",
+                afirmacao: "",
+                pontos: 0
+            },
+            {
+                texto: "Velocidade constante e aceleração nula",
+                afirmacao: "",
+                pontos: 200
+            }
+        ]
+    },
+    {
+        enunciado: "De acordo com a 1ª Lei de Newton (Princípio da Inércia), um objeto em repouso tende a: ",
+        alternativas: [
+            {
+                texto: "Permanecer em movimento com aceleração constante.",
+                afirmacao: "",
+                pontos: 0
+            },
+            {
+                texto: "Permanecer em repouso, a menos que uma força externa atue sobre ele.",
+                afirmacao: "",
+                pontos: 1
+            }            
+        ]
+    },
+    {
+        enunciado: "Qual das alternativas representa corretamente o gráfico de uma função afim, f(x)=ax+b, quando o coeficiente angular 𝑎 é negativo?",
+        alternativas: [
+            {
+                texto: "O gráfico é uma parábola com concavidade para cima..",
+                afirmacao: "",
+                pontos: 0
+            },
+            {
+                texto: "O gráfico é uma parábola com concavidade para cima.",
+                afirmacao: "",
+                pontos: 200
+            }
+        ]
+    },
+    {
+        enunciado: "Para Aristóteles, a virtude está relacionada ao:",
+        alternativas: [
+            {
+                texto: "Excesso de emoções e paixões.",
+                afirmacao: "",
+                pontos: 0
+            },
+            {
+                texto: "Meio-termo entre os extremos de comportamento.",
+                afirmacao: "",
+                pontos: 200
+            }
+        ]
+    },
+    {
+        enunciado: "A aceleração da gravidade na superfície da Terra é aproximadamente: ",
+        alternativas: [
+            {
+                texto: "8,8m/s2 ",
+                afirmacao: "",
+                pontos: 0
+            },
+            {
+                texto: "9,8𝑚/𝑠2 ",
+                afirmacao: "",
+                pontos: 200
+            }
+        ]
+    },
+    {
+        enunciado: "Qual é a função da conjunção “mas” na frase: “Estudou muito, mas não passou no exame?",
+        alternativas: [
+            {
+                texto: "Expressar contraste.",
+                afirmacao: "",
+                pontos: 200
+            },
+            {
+                texto: "Estabelecer uma conclusão",
+                afirmacao: "",
+                pontos: 0
+            }
+        ]
+    }
+];
